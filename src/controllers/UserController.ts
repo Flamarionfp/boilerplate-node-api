b06c7e6bdb072e3740294c8da0d5b760bba0b0fb
flamarionfagundes <flamarion.fagundes@lighthouseot.com.br>
2021-12-08 17:54:17 -0300

    Util de para criptografia da senha - usando no cadastro de usuários

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express'
 
 import User from '../database/schemas/User'
+import hashPassword from '../utils/hashPassword'
 class UserController {
-  public async getUsers (req: Request, res: Response): Promise<Response> {
+  public async getUsers(req: Request, res: Response): Promise<Response> {
     const users = await User.find()
     return res.json(users)
   }
 
-  public async createUser (req: Request, res: Response): Promise<Response> {
-    const user = await User.create(req.body)
+  public async createUser(req: Request, res: Response): Promise<Response> {
+    const passwordHashed = await hashPassword(req.body.password)
+    const user = await User.create({ ...req.body, password: passwordHashed })
     console.log(user)
+    console.log(passwordHashed)
     return res.json(user)
   }
 }
