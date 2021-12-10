077823bbf2f19ecc973d79e9cdafde2740118e1f
flamarionfagundes <flamarion.fagundes@lighthouseot.com.br>
2021-12-10 11:32:09 -0300

    Ajustes nas dependencias de desenvolvimento - colocando types como devDependencies

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,19 +2,18 @@ import mongoose from 'mongoose'
 import config from '../config'
 
 const { HOST, NAME } = config.database
-
 class Database {
   private host: string;
   private name: string;
 
-  constructor (host: string, name: string) {
+  constructor(host: string, name: string) {
     this.host = host
     this.name = name
   }
 
-  public connect (): void {
+  public connect(): void {
     mongoose.connect(`mongodb://${this.host}/${this.name}`).then(() => {
-      console.log(`Conexão bem sucedida ao Mongo DB - ${NAME}`)
+      console.log(`Conexão bem sucedida ao Mongo DB - ${this.name}`)
     }).catch((err) => {
       console.log(`Houve um erro ao conectar ao Mongo DB - ${err}`)
     })
