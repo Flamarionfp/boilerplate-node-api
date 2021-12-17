8680d8d65eb19b0b925c869028d3c28a684ef9d8
flamarionfagundes <flamarion.fagundes@lighthouseit.com.br>
2021-12-17 16:43:25 -0300

    Configurando o .env

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import App from './app'
-import config from './config'
 import database from './database'
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + '/.env' });
 
 database.connect()
 
-const { PORT } = config.server
+const PORT = process.env.PORT
 
 App.listen(PORT, () => {
   console.log(`App listen on ${PORT}`)
